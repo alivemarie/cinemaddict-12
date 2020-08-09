14b32b217d641f8c1ac933cc163205e3c9f3fdd4
alivemarie <alivemarie@gmail.com>
2020-08-09 11:59:27 +0300

    Обучаем шаблон отрисовки карточки фильма принимать данные

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,21 +1,37 @@
-export const createFilmCardTemplate = () => {
-  return (
-    `<article class="film-card">\n` +
-    `          <h3 class="film-card__title">Sagebrush Trail</h3>\n` +
-    `          <p class="film-card__rating">3.2</p>\n` +
-    `          <p class="film-card__info">\n` +
-    `            <span class="film-card__year">1933</span>\n` +
-    `            <span class="film-card__duration">54m</span>\n` +
-    `            <span class="film-card__genre">Western</span>\n` +
-    `          </p>\n` +
-    `          <img src="./images/posters/sagebrush-trail.jpg" alt="" class="film-card__poster">\n` +
-    `          <p class="film-card__description">Sentenced for a murder he did not commit, John Brant escapes from prison determined to find the real killer. By chance Brant's narrow escap…</p>\n` +
-    `          <a class="film-card__comments">89 comments</a>\n` +
-    `          <form class="film-card__controls">\n` +
-    `            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active">Add to watchlist</button>\n` +
-    `            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>\n` +
-    `            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>\n` +
-    `          </form>\n` +
-    `        </article>`
-  );
+import {showYearFromDate} from "../utils";
+
+export const createFilmCardTemplate = (film) => {
+  const {
+    title,
+    titleOriginal,
+    poster,
+    rating,
+    director,
+    writers,
+    actors,
+    releaseDate,
+    duration,
+    country,
+    genres,
+    description,
+    ageRating,
+    comments
+  } = film;
+  return `<article class="film-card">
+          <h3 class="film-card__title">${title}</h3>
+          <p class="film-card__rating">${rating}</p>
+          <p class="film-card__info">
+            <span class="film-card__year">${showYearFromDate(releaseDate)}</span>
+            <span class="film-card__duration">${duration}</span>
+            <span class="film-card__genre">${genres[0]}</span>
+          </p>
+          <img src="./images/posters/made-for-each-other.png" alt="" class="film-card__poster">
+          <p class="film-card__description">John Mason (James Stewart) is a young, somewhat timid attorney in New York City. He has been doing his job well, and he has a chance of bei…</p>
+          <a class="film-card__comments">56 comments</a>
+          <form class="film-card__controls">
+            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+          </form>
+        </article>`;
 };
