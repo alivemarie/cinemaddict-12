7f31ddef3c6b81823b3132d3b69b1b4e37e97438
alivemarie <alivemarie@gmail.com>
2020-08-09 21:20:27 +0300

    Исправлен шаблон для экстра колонок с фильмами

--- src/view/extra-films-list.js
+++ src/view/extra-films-list.js
@@ -1,10 +1,5 @@
 export const createExtraFilmsListTemplate = (extraHeader) => {
-  return (
-    `<section class="films-list--extra">\n      <h2 class="films-list__title">` +
-    extraHeader +
-    `</h2>\n` +
-    `      <div class="films-list__container">\n` +
-    `      </div>\n` +
-    `    </section>`
-  );
+  return `<section class="films-list--extra">
+            <h2 class="films-list__title">${extraHeader}</h2>
+            <div class="films-list__container"></div></section>`;
 };
