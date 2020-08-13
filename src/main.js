71b52671c41f60e95d7cf5d33765dd879fe98964
alivemarie <alivemarie@gmail.com>
2020-08-13 22:32:03 +0300

    Фиксы-4

--- src/main.js
+++ src/main.js
@@ -77,13 +77,13 @@ render(filmsElement, createExtraFilmsListTemplate(EXTRA_FILMS.MOST_COMMENTED));
 const topCommented = getTopCommentedFilms(allMovies);
 if (topCommented.length > 0) {
   const mostCommentedFilmsContainerElement = filmsElement.lastElementChild.querySelector(`.films-list__container`);
-  fillFilmsContainer(mostCommentedFilmsContainerElement, FILMS_COUNT.EXTRA_MOVIES, topRated);
+  fillFilmsContainer(mostCommentedFilmsContainerElement, FILMS_COUNT.EXTRA_MOVIES, topCommented);
 }
 
 const topRated = getTopRatedFilms(allMovies);
 if (topRated.length > 0) {
   const topRatedFilmsContainerElement = filmsElement.lastElementChild.previousElementSibling.querySelector(`.films-list__container`);
-  fillFilmsContainer(topRatedFilmsContainerElement, FILMS_COUNT.EXTRA_MOVIES, topCommented);
+  fillFilmsContainer(topRatedFilmsContainerElement, FILMS_COUNT.EXTRA_MOVIES, topRated);
 }
 
 const footer = document.querySelector(`footer`);
