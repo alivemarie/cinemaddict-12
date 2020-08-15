1542d3383fa243c66487fc3da77eb609a45d2cd7
alivemarie <alivemarie@gmail.com>
2020-08-15 22:43:12 +0300

    Описаны остальные компоненты как классы, и отрисованы их экземпляры на страницу

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import ProfileView from "./view/profile.js";
-import {createNavigationTemplate} from "./view/navigation.js";
+import NavigationView from "./view/navigation.js";
 import SortView from "./view/sort.js";
 import FilmsListView from "./view/films-list.js";
 import FilmCardView from "./view/film-card.js";
 import ExtraFilmsListView from "./view/extra-films-list.js";
 import ShowMoreButtonView from "./view/show-more-button.js";
 import FooterStatisticsView from "./view/footer-statistics.js";
-import {createFilmDetailsTemplate} from "./view/film-details";
-// import {createLoadingBarTemplate} from "./view/loading-bar.js";
+import FilmDetailsView from "./view/film-details";
+// import LoadingBarView from "./view/loading-bar.js";
 import {generateFilmDetails} from "./mock/film";
-import {getRandomInteger, renderTemplate, renderElement, RenderPosition} from "./utils";
+import {getRandomInteger, renderElement, RenderPosition} from "./utils";
 import {getUserRating} from "./mock/user";
 import {generateFilter} from "./mock/filter";
 import {getTopCommentedFilms, getTopRatedFilms} from "./mock/extra-films";
@@ -41,13 +41,12 @@ const userRating = getUserRating(allMovies);
 renderElement(siteHeaderElement, new ProfileView(userRating).getElement());
 
 const siteMainElement = document.querySelector(`main`);
-renderTemplate(siteMainElement, createNavigationTemplate(filters));
+renderElement(siteMainElement, new NavigationView(filters).getElement());
 renderElement(siteMainElement, new SortView().getElement());
 const filmsListComponent = new FilmsListView();
 renderElement(siteMainElement, filmsListComponent.getElement());
 
-const filmsElement = siteMainElement.querySelector(`.films`);
-const allFilmsListContainerElement = filmsElement.querySelector(`.films-list__container`);
+const allFilmsListContainerElement = filmsListComponent.getElement().querySelector(`.films-list__container`);
 const renderNumber = Math.min(allMovies.length, FILM_COUNT_PER_STEP);
 fillFilmsContainer(allFilmsListContainerElement, renderNumber, allMovies);
 
@@ -60,7 +59,7 @@ if (allMovies.length > FILM_COUNT_PER_STEP) {
     evt.preventDefault();
     allMovies
       .slice(renderedFilmsCount, renderedFilmsCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => renderTemplate(allFilmsListContainerElement, createFilmCardTemplate(film)));
+      .forEach((film) => renderElement(allFilmsListContainerElement, new FilmCardView(film).getElement()));
 
     renderedFilmsCount += FILM_COUNT_PER_STEP;
 
@@ -71,23 +70,23 @@ if (allMovies.length > FILM_COUNT_PER_STEP) {
   });
 }
 
-renderElement(filmsElement, new ExtraFilmsListView(EXTRA_FILMS.TOP_RATED).getElement());
-renderElement(filmsElement, new ExtraFilmsListView(EXTRA_FILMS.MOST_COMMENTED).getElement());
+renderElement(filmsListComponent.getElement(), new ExtraFilmsListView(EXTRA_FILMS.TOP_RATED).getElement());
+renderElement(filmsListComponent.getElement(), new ExtraFilmsListView(EXTRA_FILMS.MOST_COMMENTED).getElement());
 
 const topCommented = getTopCommentedFilms(allMovies);
 if (topCommented.length > 0) {
-  const mostCommentedFilmsContainerElement = filmsElement.lastElementChild.querySelector(`.films-list__container`);
+  const mostCommentedFilmsContainerElement = filmsListComponent.getElement().lastElementChild.querySelector(`.films-list__container`);
   fillFilmsContainer(mostCommentedFilmsContainerElement, FILMS_COUNT.EXTRA_MOVIES, topCommented);
 }
 
 const topRated = getTopRatedFilms(allMovies);
 if (topRated.length > 0) {
-  const topRatedFilmsContainerElement = filmsElement.lastElementChild.previousElementSibling.querySelector(`.films-list__container`);
+  const topRatedFilmsContainerElement = filmsListComponent.getElement().lastElementChild.previousElementSibling.querySelector(`.films-list__container`);
   fillFilmsContainer(topRatedFilmsContainerElement, FILMS_COUNT.EXTRA_MOVIES, topRated);
 }
 
-const footer = document.querySelector(`footer`);
-const footerStatistics = footer.querySelector(`.footer__statistics`);
+const footerStatistics = document.querySelector(`.footer__statistics`);
+const bodyElement = document.querySelector(`body`);
 
-// renderTemplate(footer, createFilmDetailsTemplate(allMovies[0]), `afterend`);
+renderElement(bodyElement, new FilmDetailsView(allMovies[0]).getElement());
 renderElement(footerStatistics, new FooterStatisticsView(getRandomInteger(100000, 200000)).getElement(), RenderPosition.AFTERBEGIN);
